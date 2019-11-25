207350121dba020ec50c7a59cdd4b00512facb97
marcosschroh <schrohm@gmail.com>
2019-11-25 17:48:49 +0100

    test: Test docker fixed

--- scripts/test_docker.sh
+++ scripts/test_docker.sh
@@ -5,7 +5,8 @@ set -o errexit
 project_name="my_awesome_faust_project"
 
 # install test requirements
-pip install cookiecutter==1.6.0
+pip install faust==1.9.0 simple-settings==0.16.0
+pip install -r ./requirements.txt
 
 # create a cache directory
 mkdir -p .cache/docker
@@ -19,5 +20,8 @@ docker-compose stop
 echo yes | docker-compose rm
 docker network rm ${project_name} | true
 
-# run the project's. Install tox and run the tests.
-docker-compose run -e SIMPLE_SETTINGS=${project_name}.settings ${project_name} pip install tox; tox
+ls -la
+
+./scripts/test.sh
+
+docker-compose run -e SIMPLE_SETTINGS=${project_name}.settings ${project_name} echo "project running... :-)"
